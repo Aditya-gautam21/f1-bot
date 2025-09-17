9309006ab738ae901ca8b8641e96de71f858e7b1
Aditya-gautam21 <153100646+Aditya-gautam21@users.noreply.github.com>
2025-09-17 11:59:05 +0530

    update

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -2,6 +2,23 @@ import { DataAPIClient } from "@datastax/astra-db-ts";
 import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
 import OpenAI from "openai";
 
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitters";
 
 import "dotenv/config";
+
+const{
+    ASTRA_DB_NAMESPACE,
+    ASTRA_DB_COLLECTION,
+    ASTRA_DB_API_ENDPOINT,
+    ASTRA_DB_APPLICATION_TOKEN,
+    OPENAI_API_KEY
+} = process.env
+
+const openai = new OpenAI({apiKey: OPENAI_API_KEY})
+
+const f1Data = [
+    'https://en.wikipedia.org/wiki/Formula_One'
+]
+
+const client = new DataAPIClient(ASTRA_DB_APPLICATION_TOKEN)
+const db = client.db(ASTRA_DB_API_ENDPOINT, {namespace: ASTRA_DB_NAMESPACE })
